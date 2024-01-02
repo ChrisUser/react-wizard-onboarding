e0169da7278b574eaa684a589c629e591b8775e7
ChrisUser <cristiano-raimondi@tiscali.it>
2024-01-02 23:19:45 +0100

    graphics: finalize demo page layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import React, { useRef, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import '../dist/style.css'
 import './pageStyle.css'
-import { Notifications } from './Icons/notifications'
+import { Notifications } from './resources/Icons/notifications'
+import { Dashboard } from './resources/Icons/dashboard'
+import { BulletList } from './resources/Icons/bullet_list'
+import { marketUnits, salesUnits, topCards, users } from './resources/constants'
+import { MoreVertical } from './resources/Icons/more_vert'
 
 const onboardingSteps = [
     {
@@ -55,43 +59,122 @@ const App = () => {
             /> */}
             {/* <StickyOnboardingWizard bounds={bounds} darkMode onboardingSteps={onboardingSteps} onStepChange={(newStep) => getBounds(newStep)} /> */}
             {/* <OnboardingStepSpotlight bounds={bounds} targetRef={targetElementRef} onFocusAnimationEnd={() => console.log('trigger ###')} /> */}
-            <div className="flexbox header">
-                <div className="flexbox header__left-section">
-                    <div className="flexbox main-logo-container">L</div>
-                    <div className="flexbox header-links-container">
-                        <a className="header-links-container__header-link active-link" href="#">
-                            Overview
-                        </a>
-                        <a className="header-links-container__header-link" href="#">
-                            Projects
-                        </a>
-                        <a className="header-links-container__header-link" href="#">
-                            Tasks
-                        </a>
-                        <a className="header-links-container__header-link" href="#">
-                            Settings
-                        </a>
+            <div className="flexbox header-wrapper">
+                <div className="flexbox header">
+                    <div className="flexbox header__left-section">
+                        <div className="flexbox main-logo-container">L</div>
+                        <div className="flexbox header-links-container">
+                            <a className="header-links-container__header-link active-link" href="#">
+                                Overview
+                            </a>
+                            <a className="header-links-container__header-link" href="#">
+                                Projects
+                            </a>
+                            <a className="header-links-container__header-link" href="#">
+                                Tasks
+                            </a>
+                            <a className="header-links-container__header-link" href="#">
+                                Settings
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div className="flexbox header__right-section">
-                    <input type="text" className="appearance-none flexbox search-bar" placeholder="Search..." />
-                    <div className="flexbox icon-container">
-                        <div className="notification-circle" />
-                        <Notifications className="icon" />
-                    </div>
-                    <div className="current-user-box">
-                        <span className="current-user-name">Mike Hawkings</span>
-                        <div className="avatar-container">
-                            <img className="avatar-container__image" src="https://ui.shadcn.com/avatars/04.png" alt="Avatar" />
+                    <div className="flexbox header__right-section">
+                        <input type="text" className="appearance-none flexbox search-bar" placeholder="Search..." />
+                        <div className="flexbox icon-container">
+                            <div className="notification-circle" />
+                            <Notifications className="icon" />
+                        </div>
+                        <div className="current-user-box">
+                            <span className="current-user-name">Mike Hawkings</span>
+                            <div className="avatar-container">
+                                <img className="avatar-container__image" src="https://ui.shadcn.com/avatars/04.png" alt="Avatar" />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="page-content">
-                <div className="flexbox tabs-selector">
-                    <div className="tabs-selector__tab selected-tab">Sales</div>
-                    <div className="tabs-selector__tab">Orders</div>
-                    <div className="tabs-selector__tab">Customers</div>
+                <div className="flexbox page-heading">
+                    <div className="flexbox tabs-selector">
+                        <div className="tabs-selector__tab selected-tab">Sales</div>
+                        <div className="tabs-selector__tab">Orders</div>
+                        <div className="tabs-selector__tab">Customers</div>
+                    </div>
+                    <div className="flexbox tabs-selector">
+                        <div className="tabs-selector__tab selected-tab">
+                            <Dashboard className="icon" />
+                        </div>
+                        <div className="tabs-selector__tab">
+                            <BulletList className="icon" />
+                        </div>
+                    </div>
+                </div>
+                <div className="flexbox page-body">
+                    <div className="top-page-grid">
+                        {topCards.map((topCard) => (
+                            <div className="flexbox top-card" key={topCard.id}>
+                                <div className="flexbox top-card__left">
+                                    <span className="top-card__left__title">{topCard.title}</span>
+                                    <span className="top-card__left__value">
+                                        {topCard.unit}
+                                        {topCard.value}
+                                    </span>
+                                    <span className="top-card__left__subtitle">{topCard.subtitle}</span>
+                                </div>
+                                <div className="flexbox top-card__right">
+                                    <div className="flexbox top-card__right__dot" />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flexbox items-row">
+                        <div className="flexbox card">
+                            <div className="flexbox card__title">Market</div>
+                            <div className="flexbox card__body">
+                                <div className="flexbox graph-container">
+                                    {marketUnits.map((mUnit) => (
+                                        <div className="graph-bar" style={{ height: `${mUnit}%` }} />
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flexbox card">
+                            <div className="flexbox card__title">Sales</div>
+                            <div className="flexbox card__body">
+                                <div className="flexbox graph-container">
+                                    {salesUnits.map((sUnit) => (
+                                        <div className="graph-bar" style={{ height: `${sUnit}%` }} />
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flexbox items-row">
+                        <div className="flexbox card">
+                            <div className="flexbox card__title">Users</div>
+                            <div className="flexbox card__body">
+                                {users.map((user) => (
+                                    <div key={user.id} className="flexbox user-row">
+                                        <div className="avatar-container">
+                                            <img className="avatar-container__image" src={user.avatar} alt="Avatar" />
+                                        </div>
+                                        <span className="user-row__username">{user.name}</span>
+                                        <div className="user-row__user-quota">
+                                            <span className="user-row__user-quota__value">{user.value} sales</span>
+                                        </div>
+                                        <div className="flexbox icon-container">
+                                            <MoreVertical className="icon" />
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flexbox items-row">
+                        <button className="appearance-none action-button" onClick={() => window.alert('nothing ...')}>
+                            Special Button
+                        </button>
+                    </div>
                 </div>
             </div>
 
---
+++ src/resources/constants.ts
@@ -0,0 +1,66 @@
+export const topCards = [
+    {
+        id: 0,
+        title: 'Total revenue',
+        subtitle: '+60% from last quarter',
+        unit: '$',
+        value: '56,236.35'
+    },
+    {
+        id: 1,
+        title: 'Sales',
+        subtitle: '+20% from last quarter',
+        unit: '+',
+        value: '14,465'
+    },
+    {
+        id: 2,
+        title: 'Subscriptions',
+        subtitle: '+160% from last quarter',
+        unit: '+',
+        value: '3465'
+    },
+    {
+        id: 3,
+        title: 'Active users',
+        subtitle: '+10% since last week',
+        unit: '+',
+        value: '5684'
+    }
+]
+
+export const users = [
+    {
+        id: 0,
+        name: 'Carol Jackson',
+        avatar: 'https://ui.shadcn.com/avatars/01.png',
+        value: '+130%'
+    },
+    {
+        id: 1,
+        name: 'Mark Lee',
+        avatar: 'https://ui.shadcn.com/avatars/02.png',
+        value: '+80%'
+    },
+    {
+        id: 2,
+        name: 'Olive Paul',
+        avatar: 'https://ui.shadcn.com/avatars/03.png',
+        value: '+100%'
+    },
+    {
+        id: 3,
+        name: 'Mike Hawkings',
+        avatar: 'https://ui.shadcn.com/avatars/04.png',
+        value: '+55%'
+    },
+    {
+        id: 4,
+        name: 'Josephine Besinger',
+        avatar: 'https://ui.shadcn.com/avatars/05.png',
+        value: '+94%'
+    }
+]
+
+export const marketUnits = ['30', '60', '85', '90', '70', '82', '45', '30']
+export const salesUnits = ['80', '50', '65', '74', '70', '82', '89', '72']
